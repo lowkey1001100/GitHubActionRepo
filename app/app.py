d70ab5c65df2261e413ad05c9d64418fa19812c4
lowkey1001100 <lowkey1001100@gmail.com>
2022-08-10 09:20:34 -0400

    Udating files

---
+++ app/app.py
@@ -0,0 +1,14 @@
+"""
+created: 2022/08/10 09:15:15
+@author: lowkey
+metadoc: Greeting message
+"""
+
+
+def greeting_message():
+    msg = 'Hello, how are you'
+    return msg
+
+
+if __name__ == '__main__':
+    pass
